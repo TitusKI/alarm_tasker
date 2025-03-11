7edfdb4b24bf49b8391fec73351524a17ed292ea
TitusKI <kidumazie@gmail.com>
2025-03-11 15:07:15 +0300

    Add ColorPickerDialog for selecting and updating primary theme color

---
+++ lib/features/tasks/presentation/widgets/color_picker.dart
@@ -0,0 +1,102 @@
+import 'package:alarm_tasker/features/theme/presentation/cubit/theme_cubit.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+
+class ColorPickerDialog extends StatefulWidget {
+  const ColorPickerDialog({super.key});
+
+  @override
+  _ColorPickerDialogState createState() => _ColorPickerDialogState();
+}
+
+class _ColorPickerDialogState extends State<ColorPickerDialog> {
+  late Color selectedColor;
+
+  @override
+  void initState() {
+    super.initState();
+    final themeState = context.read<ThemeCubit>().state;
+    selectedColor = themeState.primaryColor;
+  }
+
+  void changeColor(Color color) {
+    setState(() => selectedColor = color);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      contentPadding: EdgeInsets.zero,
+      content: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              color: selectedColor,
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('List Name',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 18)),
+                  const TextField(
+                    decoration: InputDecoration(
+                      hintText: "Let's give it a name...",
+                      hintStyle: TextStyle(color: Colors.white70),
+                      border: InputBorder.none,
+                    ),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            ColorPicker(
+              pickerColor: selectedColor,
+              onColorChanged: changeColor,
+              pickerAreaHeightPercent: 0.8,
+              enableAlpha: false,
+              displayThumbColor: true,
+              paletteType: PaletteType.hueWheel,
+              labelTypes: const [],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text('CANCEL',
+                        style: TextStyle(color: Colors.orangeAccent)),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      context
+                          .read<ThemeCubit>()
+                          .updatePrimaryColor(selectedColor);
+                      Navigator.pop(context);
+                    },
+                    child: const Text('SAVE',
+                        style: TextStyle(color: Colors.orangeAccent)),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void showColorPicker(BuildContext context) async {
+  await showDialog<Color>(
+    context: context,
+    builder: (context) => const ColorPickerDialog(),
+  );
+}
