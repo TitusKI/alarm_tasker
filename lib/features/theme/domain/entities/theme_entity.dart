d155a7bef667f43af7ad125d751deaa6957800df
TitusKI <kidumazie@gmail.com>
2025-03-11 15:07:35 +0300

    Add ThemeEntity class for managing theme properties

---
+++ lib/features/theme/domain/entities/theme_entity.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class ThemeEntity {
+  final Color primaryColor;
+  final bool isDarkMode;
+
+  const ThemeEntity({
+    required this.primaryColor,
+    required this.isDarkMode,
+  });
+}
