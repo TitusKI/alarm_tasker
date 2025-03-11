144a1269e5e91154deaf5033cb559c19ab8ff8f5
TitusKI <kidumazie@gmail.com>
2025-03-11 15:04:57 +0300

    Implement main application entry point with dependency injection and theme management

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:alarm_tasker/config/routes/routes.dart';
+import 'package:alarm_tasker/features/theme/domain/entities/theme_entity.dart';
+import 'package:alarm_tasker/features/theme/presentation/cubit/theme_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'config/theme/app_theme.dart';
+import 'features/theme/presentation/cubit/theme_cubit.dart';
+import 'injection_container.dart';
+
+void main() async {
+  await initializeDependencies();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => sl<ThemeCubit>(),
+      child: ScreenUtilInit(
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        child: BlocBuilder<ThemeCubit, ThemeState>(
+          builder: (context, state) {
+            return MaterialApp.router(
+                debugShowCheckedModeBanner: false,
+                routerConfig: Routes().router,
+                theme: getAppTheme(state.primaryColor, state.isDarkMode));
+          },
+        ),
+      ),
+    );
+  }
+}
