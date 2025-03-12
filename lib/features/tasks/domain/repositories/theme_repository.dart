19ff395fe9cabe933d4f3179b7ae9a9e3e94471a
TitusKI <kidumazie@gmail.com>
2025-03-12 16:39:41 +0300

    Update ThemeRepository to include text color management

--- lib/features/tasks/domain/repositories/theme_repository.dart
+++ lib/features/tasks/domain/repositories/theme_repository.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 abstract class ThemeRepository {
-  Future<void> updatePrimaryColor(Color color);
+  Future<void> updatePrimaryColor(Color primaryColor, Color textColor);
   Future<void> updateThemeMode(bool isDark);
   Color? getPrimaryColor();
+  Color? getTextColor();
   bool? getThemeMode();
 }
