1f5109b8204a2de3c8cda486f0a2cc8852dd31c2
TitusKI <kidumazie@gmail.com>
2025-03-12 21:25:53 +0300

    Refactor AppBarTheme to accept dynamic icon color for improved customization

--- lib/config/theme/theme_widgets/appbar_theme.dart
+++ lib/config/theme/theme_widgets/appbar_theme.dart
@@ -2,24 +2,26 @@ import 'package:flutter/material.dart';
 
 class TAppBarTheme {
   TAppBarTheme._();
-  AppBarTheme lightAppBarTheme(Color primaryColor) => AppBarTheme(
-      elevation: 0,
-      centerTitle: false,
-      scrolledUnderElevation: 0,
-      backgroundColor: primaryColor,
-      surfaceTintColor: Colors.transparent,
-      iconTheme: IconThemeData(color: Colors.white, size: 24),
-      actionsIconTheme: IconThemeData(color: Colors.white, size: 24),
-      titleTextStyle: TextStyle(
-          fontSize: 18.0, fontWeight: FontWeight.w600, color: Colors.white));
-  AppBarTheme darkAppBarTheme(Color primaryColor) => AppBarTheme(
-      elevation: 0,
-      centerTitle: false,
-      scrolledUnderElevation: 0,
-      backgroundColor: primaryColor,
-      surfaceTintColor: Colors.transparent,
-      iconTheme: IconThemeData(color: Colors.white, size: 24),
-      actionsIconTheme: IconThemeData(color: Colors.white, size: 24),
-      titleTextStyle: TextStyle(
-          fontSize: 18.0, fontWeight: FontWeight.w600, color: Colors.white));
+  static AppBarTheme lightAppBarTheme(Color primaryColor, Color iconColor) =>
+      AppBarTheme(
+          elevation: 0,
+          centerTitle: false,
+          scrolledUnderElevation: 0,
+          backgroundColor: primaryColor,
+          surfaceTintColor: Colors.transparent,
+          iconTheme: IconThemeData(color: iconColor, size: 24),
+          actionsIconTheme: IconThemeData(color: iconColor, size: 24),
+          titleTextStyle: TextStyle(
+              fontSize: 18.0, fontWeight: FontWeight.w600, color: iconColor));
+  static AppBarTheme darkAppBarTheme(Color primaryColor, Color iconColor) =>
+      AppBarTheme(
+          elevation: 0,
+          centerTitle: false,
+          scrolledUnderElevation: 0,
+          backgroundColor: primaryColor,
+          surfaceTintColor: Colors.transparent,
+          iconTheme: IconThemeData(color: iconColor, size: 24),
+          actionsIconTheme: IconThemeData(color: iconColor, size: 24),
+          titleTextStyle: TextStyle(
+              fontSize: 18.0, fontWeight: FontWeight.w600, color: iconColor));
 }
