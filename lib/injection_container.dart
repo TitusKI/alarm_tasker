168b9086fac39f2a1e2cf2ab37c167163bb1da04
TitusKI <kidumazie@gmail.com>
2025-03-12 21:25:33 +0300

    Register task-related dependencies in the injection container

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,4 +1,9 @@
 import 'package:alarm_tasker/core/util/adapters/color_adapter.dart';
+import 'package:alarm_tasker/features/tasks/data/models/subtask_model.dart';
+import 'package:alarm_tasker/features/tasks/domain/repositories/task_repository.dart';
+import 'package:alarm_tasker/features/tasks/domain/usecases/add_task.dart';
+import 'package:alarm_tasker/features/tasks/domain/usecases/get_tasks.dart';
+import 'package:alarm_tasker/features/tasks/domain/usecases/update_task.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
@@ -7,12 +12,16 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path/path.dart' as path;
 import 'package:flutter/foundation.dart';
 
+import 'features/tasks/data/datasources/task_local_data_source.dart';
+import 'features/tasks/data/models/task_model.dart';
+import 'features/tasks/data/repositories/task_repository_impl.dart';
+import 'features/tasks/domain/usecases/delete_task.dart';
 import 'features/theme/data/datasources/theme_local_data_source.dart';
-import 'features/tasks/data/repositories/theme_repository_impl.dart';
-import 'features/tasks/domain/repositories/theme_repository.dart';
-import 'features/tasks/domain/usecases/get_theme.dart';
-import 'features/tasks/domain/usecases/toggle_theme_mode.dart';
-import 'features/tasks/domain/usecases/update_theme.dart';
+import 'features/theme/data/repositories/theme_repository_impl.dart';
+import 'features/theme/domain/repositories/theme_repository.dart';
+import 'features/theme/domain/usecases/get_theme.dart';
+import 'features/theme/domain/usecases/toggle_theme_mode.dart';
+import 'features/theme/domain/usecases/update_theme.dart';
 import 'features/theme/presentation/cubit/theme_cubit.dart';
 
 final GetIt sl = GetIt.instance;
@@ -27,17 +36,27 @@ Future<void> initializeDependencies() async {
   }
   // Register the colorAdapter
   Hive.registerAdapter(ColorAdapter());
+  Hive.registerAdapter(TaskAdapter());
+  Hive.registerAdapter(SubTaskAdapter());
   // Open the box
   final box = await Hive.openBox('theme_box');
+  final taskBox = await Hive.openBox<Task>('tasks_box');
   // Data Sources
   sl.registerLazySingleton<ThemeLocalDataSource>(
       () => ThemeLocalDataSourceImpl(box));
+  sl.registerLazySingleton(() => TaskLocalDataSource(taskBox));
   // Repositories,
   sl.registerLazySingleton<ThemeRepository>(() => ThemeRepositoryImpl());
+  sl.registerLazySingleton<TaskRepository>(() => TaskRepositoryImpl());
   // Use cases
   sl.registerLazySingleton(() => GetThemeUsecase());
   sl.registerLazySingleton(() => UpdateThemeUsecase());
   sl.registerLazySingleton(() => ToggleThemeModeUsecase());
+  // task usecases
+  sl.registerLazySingleton(() => AddTaskUsecase());
+  sl.registerLazySingleton(() => DeleteTaskUsecase());
+  sl.registerLazySingleton(() => UpdateTaskUsecase());
+  sl.registerLazySingleton(() => GetTasksUsecase());
   // Cubits
   sl.registerFactory(() => ThemeCubit());
 }
