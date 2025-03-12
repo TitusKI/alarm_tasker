7cf33d013c1dc63a1b550c91e53f574391ee23d6
TitusKI <kidumazie@gmail.com>
2025-03-12 16:41:47 +0300

    Add textColor property to ThemeState and update copyWith method

--- lib/features/theme/presentation/cubit/theme_state.dart
+++ lib/features/theme/presentation/cubit/theme_state.dart
@@ -2,17 +2,21 @@ import 'package:flutter/material.dart';
 
 class ThemeState {
   final Color primaryColor;
+  final Color textColor;
   final bool isDarkMode;
 
   ThemeState({
     required this.primaryColor,
     required this.isDarkMode,
+    required this.textColor,
   });
 
-  ThemeState copyWith({Color? primaryColor, bool? isDarkMode}) {
+  ThemeState copyWith(
+      {Color? primaryColor, bool? isDarkMode, Color? textColor}) {
     return ThemeState(
       primaryColor: primaryColor ?? this.primaryColor,
       isDarkMode: isDarkMode ?? this.isDarkMode,
+      textColor: textColor ?? this.textColor,
     );
   }
 }
