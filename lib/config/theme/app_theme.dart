ddec0bcdba396a496abb9d30e022afa2b2bd688b
TitusKI <kidumazie@gmail.com>
2025-03-12 16:38:19 +0300

    Enhance app theme by adding Google Fonts support and updating getAppTheme parameters

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-ThemeData getAppTheme(Color primaryColor, bool isDarkMode) {
+import 'theme_widgets/text_theme.dart';
+
+ThemeData getAppTheme(
+    {required Color primaryColor,
+    required bool isDarkMode,
+    required textColor}) {
   return ThemeData(
     brightness: isDarkMode ? Brightness.dark : Brightness.light,
     primaryColor: primaryColor,
@@ -10,6 +16,12 @@ ThemeData getAppTheme(Color primaryColor, bool isDarkMode) {
     floatingActionButtonTheme: FloatingActionButtonThemeData(
       backgroundColor: primaryColor,
     ),
+    textTheme:
+        GoogleFonts.robotoTextTheme(TTextTheme.lightTextTheme(textColor)),
+    // setting drawer header theme of my app
+
+    // settng box decoration theme of my app
+
     colorScheme: ColorScheme.fromSeed(
         seedColor: primaryColor,
         brightness: isDarkMode ? Brightness.dark : Brightness.light),
