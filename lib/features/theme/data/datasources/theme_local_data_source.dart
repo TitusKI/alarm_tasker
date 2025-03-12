fbdf5c6720c597d9a0b0e27dd6548ff19e82e001
TitusKI <kidumazie@gmail.com>
2025-03-12 16:39:23 +0300

    Add ThemeLocalDataSource for caching theme-related data

--- lib/features/theme/data/datasources/theme_local_data_source.dart
+++ lib/features/theme/data/datasources/theme_local_data_source.dart
@@ -3,8 +3,12 @@ import 'package:hive/hive.dart';
 
 abstract class ThemeLocalDataSource {
   Future<void> cachePrimaryColor(Color color);
+  Future<void> cacheTextColor(Color color);
+
   Future<void> cacheThemeMode(bool isDark);
   Color? getPrimaryColor();
+  Color? getTextColor();
+
   bool? getThemeMode();
 }
 
@@ -14,11 +18,18 @@ class ThemeLocalDataSourceImpl implements ThemeLocalDataSource {
   ThemeLocalDataSourceImpl(this._box);
 
   static const String primaryColorKey = 'primary_color';
+  static const String textColorKey = 'text_color';
+
   static const String themeModeKey = 'is_dark_mode';
 
   @override
-  Future<void> cachePrimaryColor(Color color) async {
-    await _box.put(primaryColorKey, color);
+  Future<void> cachePrimaryColor(Color primaryColor) async {
+    await _box.put(primaryColorKey, primaryColor);
+  }
+
+  @override
+  Future<void> cacheTextColor(Color textColor) async {
+    await _box.put(textColorKey, textColor);
   }
 
   @override
@@ -29,7 +40,13 @@ class ThemeLocalDataSourceImpl implements ThemeLocalDataSource {
   @override
   Color? getPrimaryColor() {
     final colorValue = _box.get(primaryColorKey);
-    return colorValue != null ? Color(colorValue) : null;
+    return colorValue != null ? colorValue as Color : null;
+  }
+
+  @override
+  Color? getTextColor() {
+    final colorValue = _box.get(textColorKey);
+    return colorValue != null ? colorValue as Color : null;
   }
 
   @override
