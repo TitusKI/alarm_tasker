c74cca692d3a360938cbbf9d33aa459dc43302b8
TitusKI <kidumazie@gmail.com>
2025-03-12 21:28:59 +0300

    Add TaskRepository interface for task management operations

---
+++ lib/features/tasks/domain/repositories/task_repository.dart
@@ -0,0 +1,8 @@
+import '../entities/task.dart';
+
+abstract class TaskRepository {
+  Future<List<TaskEntity>> getTasks();
+  Future<void> addTask(TaskEntity task);
+  Future<void> updateTask(TaskEntity task);
+  Future<void> deleteTask(String taskId);
+}
