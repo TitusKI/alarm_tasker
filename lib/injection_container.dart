318159b368316950ef479d9ef3c3e43439ec22b4
TitusKI <kidumazie@gmail.com>
2025-03-12 16:37:51 +0300

    Register ColorAdapter and update theme local data source import path

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,13 +1,13 @@
+import 'package:alarm_tasker/core/util/adapters/color_adapter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
-import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path/path.dart' as path;
 import 'package:flutter/foundation.dart';
 
-import 'features/tasks/data/datasources/theme_local_data_source.dart';
+import 'features/theme/data/datasources/theme_local_data_source.dart';
 import 'features/tasks/data/repositories/theme_repository_impl.dart';
 import 'features/tasks/domain/repositories/theme_repository.dart';
 import 'features/tasks/domain/usecases/get_theme.dart';
@@ -20,13 +20,14 @@ Future<void> initializeDependencies() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   if (kIsWeb) {
-    // For web, use IndexedDB as the storage backend
     await Hive.initFlutter();
   } else {
-    // For mobile/desktop, use the local filesystem
     final appDocumentDirectory = await getApplicationDocumentsDirectory();
     Hive.init(path.join(appDocumentDirectory.path, 'hive'));
   }
+  // Register the colorAdapter
+  Hive.registerAdapter(ColorAdapter());
+  // Open the box
   final box = await Hive.openBox('theme_box');
   // Data Sources
   sl.registerLazySingleton<ThemeLocalDataSource>(
