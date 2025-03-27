1741d97eb4450c17f813f8672ad0196f1257e73e
TitusKI <kidumazie@gmail.com>
2025-03-27 10:19:41 +0300

    Add stream controller to SubTaskCubit and enhance logging for subtask updates

--- lib/features/tasks/presentation/cubit/subtasks_cubit.dart
+++ lib/features/tasks/presentation/cubit/subtasks_cubit.dart
@@ -1,12 +1,27 @@
+import 'package:alarm_tasker/features/tasks/data/models/task_w_subtask.dart';
 import 'package:alarm_tasker/features/tasks/domain/usecases/subtask_usecase.dart';
+import 'package:alarm_tasker/features/tasks/presentation/cubit/tasks_w_subtask_cubit.dart';
 import 'package:bloc/bloc.dart';
 
 import '../../../../core/resources/generic_state.dart';
 import '../../../../injection_container.dart';
 import '../../domain/entities/sub_task.dart';
 
+import 'dart:async';
+
 class SubTaskCubit extends Cubit<GenericState<List<SubTaskEntity>>> {
+  final StreamController<List<SubTaskEntity>> _streamController =
+      StreamController<List<SubTaskEntity>>.broadcast();
+
   SubTaskCubit() : super(GenericState.initial());
+
+  Stream<List<SubTaskEntity>> get streamcontroler => _streamController.stream;
+
+  @override
+  Future<void> close() {
+    _streamController.close();
+    return super.close();
+  }
 
   Future<void> loadSubTasks(String subTaskId) async {
     emit(GenericState.loading());
@@ -40,19 +55,28 @@ class SubTaskCubit extends Cubit<GenericState<List<SubTaskEntity>>> {
 
   Future<void> updateSubTask(SubTaskEntity subTask) async {
     try {
+      print("Starting update subtask...");
+      print(
+          "subTask id and isCompleted: ${subTask.id} and ${subTask.isCompleted}");
       await sl<UpdateSubTaskUsecase>().call(params: subTask);
+      print("Successfully updated subtask");
       final updatedList = (state.data ?? []).map((task) {
         return task.id == subTask.id ? subTask : task;
       }).toList();
+      print("updatedList: $updatedList");
       emit(GenericState.success(updatedList));
+      // sl<TasksWSubtaskCubit>().loadTasksWithSubTasks(id: subTask.id);
     } catch (e) {
+      print("Error catched: $e");
       emit(GenericState.failure(e.toString()));
     }
   }
 
   Future<void> deleteSubTask(String subTaskId) async {
     try {
+      print("Starting to delete ");
       await sl<DeleteSubTaskUsecase>().call(params: subTaskId);
+      print("Successfully deleted subtask");
       final updatedList =
           (state.data ?? []).where((task) => task.id != subTaskId).toList();
       emit(GenericState.success(updatedList));
