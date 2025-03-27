e1a43335b67047276ba11f77d1d1d1714bda5bd3
TitusKI <kidumazie@gmail.com>
2025-03-27 10:19:33 +0300

    Make title and completedAt fields nullable in SubTaskEntity

--- lib/features/tasks/domain/entities/sub_task.dart
+++ lib/features/tasks/domain/entities/sub_task.dart
@@ -1,21 +1,23 @@
 class SubTaskEntity {
   final String id;
-  final String title;
+  final String? title;
   final DateTime? dueDate;
   final String? priority;
   final String? imagePath;
   final String? description;
-  final bool isCompleted;
+  bool isCompleted;
+  DateTime? completedAt;
   final String subTaskTitleId;
 
   SubTaskEntity({
     required this.id,
-    required this.title,
+    this.title,
     this.dueDate,
     this.isCompleted = false,
     this.priority,
     this.imagePath,
     this.description,
+    this.completedAt,
     required this.subTaskTitleId,
   });
 }
