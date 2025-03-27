6f24283014e7627f951b4f29164b19be005fee02
TitusKI <kidumazie@gmail.com>
2025-03-27 10:19:10 +0300

    Refactor TaskWithSubTasks to use SubTaskModel and comment out legacy SubTask class

--- lib/features/tasks/data/models/task_w_subtask.dart
+++ lib/features/tasks/data/models/task_w_subtask.dart
@@ -1,3 +1,5 @@
+import 'subtask_model.dart';
+
 class TaskWithSubTasks {
   final String id;
   final String title;
@@ -13,7 +15,7 @@ class TaskWithSubTasks {
 class SubTaskTitleWithSubtasks {
   final String id;
   final String title;
-  final List<SubTask> subtasks;
+  final List<SubTaskModel> subtasks;
 
   SubTaskTitleWithSubtasks({
     required this.id,
@@ -22,54 +24,59 @@ class SubTaskTitleWithSubtasks {
   });
 }
 
-class SubTask {
-  final String id;
-  final String title;
-  final String? dueDate;
-  final String? priority;
-  final String? imagePath;
-  final String? description;
-  final bool isCompleted;
+// class SubTask {
+//   final String id;
+//   final String title;
+//   final String? dueDate;
+//   final String? priority;
+//   final String? imagePath;
+//   final String? description;
+//   final bool isCompleted;
+//   final String? subtaskTitleId;
 
-  SubTask({
-    required this.id,
-    required this.title,
-    this.dueDate,
-    this.priority,
-    this.imagePath,
-    this.description,
-    required this.isCompleted,
-  });
+//   SubTask({
+//     required this.id,
+//     required this.title,
+//     this.dueDate,
+//     this.priority,
+//     this.imagePath,
+//     this.description,
+//     required this.isCompleted,
+//     this.subtaskTitleId,
+//   });
 
-  factory SubTask.fromMap(Map<String, dynamic> map) {
-    return SubTask(
-      id: map['subTaskId'],
-      title: map['subTaskName'],
-      dueDate: map['dueDate'],
-      priority: map['priority'],
-      imagePath: map['imagePath'],
-      description: map['description'],
-      isCompleted: map['isCompleted'] == 1,
-    );
-  }
-  // copyWith Method for updating subTask
-  SubTask copyWith({
-    String? id,
-    String? title,
-    String? dueDate,
-    String? priority,
-    String? imagePath,
-    String? description,
-    bool? isCompleted,
-  }) {
-    return SubTask(
-      id: id ?? this.id,
-      title: title ?? this.title,
-      dueDate: dueDate ?? this.dueDate,
-      priority: priority ?? this.priority,
-      imagePath: imagePath ?? this.imagePath,
-      description: description ?? this.description,
-      isCompleted: isCompleted ?? this.isCompleted,
-    );
-  }
-}
+//   factory SubTask.fromMap(Map<String, dynamic> map) {
+//     return SubTask(
+//       id: map['subTaskId'],
+//       title: map['subTaskName'],
+//       dueDate: map['dueDate'],
+//       priority: map['priority'],
+//       imagePath: map['imagePath'],
+//       description: map['description'],
+//       isCompleted: map['isCompleted'] == 1,
+//       subtaskTitleId: map['subTaskTitleId'],
+//     );
+//   }
+//   // copyWith Method for updating subTask
+//   SubTask copyWith({
+//     String? id,
+//     String? title,
+//     String? dueDate,
+//     String? priority,
+//     String? imagePath,
+//     String? description,
+//     bool? isCompleted,
+//     String? subtaskTitleId,
+//   }) {
+//     return SubTask(
+//       id: id ?? this.id,
+//       title: title ?? this.title,
+//       dueDate: dueDate ?? this.dueDate,
+//       priority: priority ?? this.priority,
+//       imagePath: imagePath ?? this.imagePath,
+//       description: description ?? this.description,
+//       isCompleted: isCompleted ?? this.isCompleted,
+//       subtaskTitleId: subtaskTitleId ?? this.subtaskTitleId,
+//     );
+//   }
+// }
