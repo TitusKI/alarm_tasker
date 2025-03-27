1f9b70e558155a501763ec9446b32fadb04193fe
TitusKI <kidumazie@gmail.com>
2025-03-27 10:20:17 +0300

    Refactor Drawer widget to enhance UI and integrate system UI overlay style

--- lib/features/tasks/presentation/widgets/drawer.dart
+++ lib/features/tasks/presentation/widgets/drawer.dart
@@ -1,5 +1,6 @@
 import 'package:alarm_tasker/features/tasks/presentation/widgets/color_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 
@@ -8,60 +9,72 @@ import '../../../theme/presentation/cubit/theme_cubit.dart';
 
 Widget drawer(BuildContext context) {
   return Drawer(
-    clipBehavior: Clip.none,
+    shape: BeveledRectangleBorder(),
+    clipBehavior: Clip.hardEdge,
     width: 280.w,
-    child: ListView(
-      children: <Widget>[
-        SizedBox(
-          height: 50.h,
-          child: DrawerHeader(
-            margin: EdgeInsets.zero,
-            decoration: BoxDecoration(
-              color: sl<ThemeCubit>().state.primaryColor,
-            ),
-            child: Text(
-              textAlign: TextAlign.left,
-              textDirection: TextDirection.rtl,
-              'Tasks',
-              style: Theme.of(context).textTheme.headlineSmall,
-            ),
+    child: Builder(
+      builder: (context) {
+        final primaryColor = sl<ThemeCubit>().state.primaryColor;
+        return AnnotatedRegion<SystemUiOverlayStyle>(
+          value: SystemUiOverlayStyle(
+            statusBarColor: primaryColor,
+            statusBarIconBrightness: Brightness.light,
           ),
-        ),
-        ListTile(
-          onTap: () {
-            Navigator.of(context).pop(); // Close the drawer
-            // context.go('/'); // Move to task screen using go router
-            showColorPicker(context);
-          },
-          leading: Icon(
-            Icons.add_circle,
+          child: ListView(
+            children: <Widget>[
+              SizedBox(
+                height: 50.h,
+                child: DrawerHeader(
+                  margin: EdgeInsets.zero,
+                  decoration: BoxDecoration(
+                    color: primaryColor,
+                  ),
+                  child: Text(
+                    textAlign: TextAlign.left,
+                    textDirection: TextDirection.rtl,
+                    'Tasks',
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                ),
+              ),
+              ListTile(
+                onTap: () {
+                  Navigator.of(context).pop(); // Close the drawer
+                  // context.go('/'); // Move to task screen using go router
+                  showColorPicker(context);
+                },
+                leading: Icon(
+                  Icons.add_circle,
+                ),
+                horizontalTitleGap: 35.w,
+                title: Text('New List'),
+              ),
+              Divider(
+                thickness: 0.5.h,
+              ),
+              ListTile(
+                leading: Icon(Icons.favorite),
+                horizontalTitleGap: 35.w,
+                title: Text('Donate'),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.help,
+                ),
+                horizontalTitleGap: 35.w,
+                title: Text('Help'),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.settings,
+                ),
+                horizontalTitleGap: 35.w,
+                title: Text('Settings'),
+              ),
+            ],
           ),
-          horizontalTitleGap: 35.w,
-          title: Text('New List'),
-        ),
-        Divider(
-          thickness: 0.5.h,
-        ),
-        ListTile(
-          leading: Icon(Icons.favorite),
-          horizontalTitleGap: 35.w,
-          title: Text('Donate'),
-        ),
-        ListTile(
-          leading: Icon(
-            Icons.help,
-          ),
-          horizontalTitleGap: 35.w,
-          title: Text('Help'),
-        ),
-        ListTile(
-          leading: Icon(
-            Icons.settings,
-          ),
-          horizontalTitleGap: 35.w,
-          title: Text('Settings'),
-        ),
-      ],
+        );
+      },
     ),
   );
 }
