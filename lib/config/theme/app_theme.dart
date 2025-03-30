d5ccc2da8a357af9515868a0a4ae1ce14a994f89
TitusKI <kidumazie@gmail.com>
2025-03-30 21:49:08 +0300

    Enhance app theme with date and time picker customization

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,29 +1,67 @@
 import 'package:alarm_tasker/config/theme/theme_widgets/appbar_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'theme_widgets/text_theme.dart';
 
-ThemeData getAppTheme(
-    {required Color primaryColor,
-    required bool isDarkMode,
-    required textColor}) {
+ThemeData getAppTheme({
+  required Color primaryColor,
+  required bool isDarkMode,
+  required Color textColor,
+}) {
   return ThemeData(
     brightness: isDarkMode ? Brightness.dark : Brightness.light,
     primaryColor: primaryColor,
     appBarTheme: TAppBarTheme.lightAppBarTheme(primaryColor, textColor),
-
     floatingActionButtonTheme: FloatingActionButtonThemeData(
       backgroundColor: primaryColor,
     ),
     textTheme:
         GoogleFonts.robotoTextTheme(TTextTheme.lightTextTheme(textColor)),
-    // setting drawer header theme of my app
-
-    // settng box decoration theme of my app
-
+    // Date Picker Theme
+    datePickerTheme: DatePickerThemeData(
+      shape: LinearBorder(),
+      backgroundColor: isDarkMode ? Colors.black : Colors.white,
+      headerBackgroundColor: primaryColor,
+      headerForegroundColor: Colors.white,
+      todayBackgroundColor: WidgetStateProperty.all(primaryColor.withAlpha(20)),
+      todayForegroundColor: WidgetStateProperty.all(primaryColor),
+      yearBackgroundColor:
+          WidgetStateProperty.all(primaryColor.withOpacity(0.1)),
+      yearForegroundColor: WidgetStateProperty.all(primaryColor),
+    ),
+// Time Picker Theme
+    timePickerTheme: TimePickerThemeData(
+      helpTextStyle: TextStyle(
+        color: isDarkMode ? Colors.white : Colors.black,
+      ),
+      shape: LinearBorder(),
+      // backgroundColor: isDarkMode ? Colors.black : Colors.white,
+      hourMinuteColor: Colors.transparent,
+      hourMinuteTextColor: WidgetStateColor.resolveWith((states) {
+        return states.contains(WidgetState.selected)
+            ? (isDarkMode ? Colors.white : Colors.black)
+            : (isDarkMode
+                ? Colors.white.withAlpha(50)
+                : Colors.black.withAlpha(50));
+      }),
+      dialHandColor: primaryColor,
+      dialTextColor: isDarkMode ? Colors.white : Colors.black,
+      entryModeIconColor: primaryColor,
+      dayPeriodBorderSide: BorderSide.none,
+      dayPeriodColor: Colors.transparent,
+      dayPeriodTextColor: WidgetStateColor.resolveWith((states) {
+        return states.contains(WidgetState.selected)
+            ? (isDarkMode ? Colors.white : Colors.black)
+            : (isDarkMode
+                ? Colors.white.withAlpha(50)
+                : Colors.black.withAlpha(50));
+      }),
+    ),
     colorScheme: ColorScheme.fromSeed(
-        seedColor: primaryColor,
-        brightness: isDarkMode ? Brightness.dark : Brightness.light),
+      seedColor: primaryColor,
+      brightness: isDarkMode ? Brightness.dark : Brightness.light,
+    ),
   );
 }
